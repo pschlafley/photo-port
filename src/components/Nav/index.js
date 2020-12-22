bf918837d1b448e3239cf23b11333c2fa93c1355
Peyton Schlafley <pschlafley0@gmail.com>
2020-12-22 14:48:46 -0500

    added tests for the nav emoji and links being rendered

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -28,14 +28,14 @@ const Nav = () => {
     return(
         <header>
             <h2>
-                <a href="/">
+                <a href="/" data-testid="link">
                     <span role="img" aria-label="camera"> 📸</span> Oh Snap!
                 </a>
             </h2>
             <nav>
                 <ul className="flex-row">
                     <li className="mx-2">
-                        <a href="#about">
+                        <a href="#about" data-testid="about">
                             About Me
                         </a>
                     </li>
