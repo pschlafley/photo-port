1ab9682a98be22b746d6664267ae7de41b1e56ce
Peyton Schlafley <pschlafley0@gmail.com>
2020-12-26 12:03:50 -0500

    created a modal for the use to see a larger form of an image that they click on

---
+++ src/components/Modal/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Modal({ onClose, currentPhoto }) {
+
+    const { name, category, description, index } = currentPhoto;
+
+    return (
+        <div className="modalBackdrop">
+            <div className="modalContainer">
+                <h3 className="modalTitle">{name}</h3>
+                <img alt="current category" src={require(`../../assets/large/${category}/${index}.jpg`)} />
+                <p>
+                    {description}
+                </p>
+                <button onClick={onClose} type="button">
+                    Close this modal
+                </button>
+            </div>
+        </div>
+
+    );
+}
+
+export default Modal;
